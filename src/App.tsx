d7776b78b669ed76eddbbc0ea7dcec20e6634d78
kimjuwon <juwon2298@naver.com>
2023-06-09 20:49:50 +0900

    👀 :: App 컴포넌트 수정

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,6 @@ import { BrowserRouter } from "react-router-dom";
 import { RecoilRoot } from "recoil";
 import { QueryClientProvider, QueryClient } from "react-query";
 import { GlobalStyle } from "./styles/GlobalStyle";
-import { ThemeProvider } from "styled-components";
-import { theme } from "./styles/Theme";
 import MainRouter from "./router/MainRouter";
 
 const queryClient = new QueryClient({
@@ -20,14 +18,12 @@ const queryClient = new QueryClient({
 function App() {
   return (
     <QueryClientProvider client={queryClient}>
-      <ThemeProvider theme={theme}>
-        <GlobalStyle />
-        <RecoilRoot>
-          <BrowserRouter>
-            <MainRouter />
-          </BrowserRouter>
-        </RecoilRoot>
-      </ThemeProvider>
+      <GlobalStyle />
+      <RecoilRoot>
+        <BrowserRouter>
+          <MainRouter />
+        </BrowserRouter>
+      </RecoilRoot>
     </QueryClientProvider>
   );
 }
