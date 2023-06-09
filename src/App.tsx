91fb196eca12216e78ec0cc98c3b6ea6cc677f15
kimjuwon <juwon2298@naver.com>
2023-06-09 12:10:26 +0900

    💡:: 프로젝트 생성 및 세팅

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,35 @@
-import React from "react";
+import { BrowserRouter } from "react-router-dom";
+import { RecoilRoot } from "recoil";
+import { QueryClientProvider, QueryClient } from "react-query";
+import { GlobalStyle } from "./styles/GlobalStyle";
+import { ThemeProvider } from "styled-components";
+import { theme } from "./styles/Theme";
+import MainRouter from "./router/MainRouter";
 
-const App = () => {
-  return <div>App</div>;
-};
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      retry: 0,
+      staleTime: 1000,
+      refetchInterval: 0,
+      refetchOnWindowFocus: false,
+    },
+  },
+});
+
+function App() {
+  return (
+    <QueryClientProvider client={queryClient}>
+      <ThemeProvider theme={theme}>
+        <GlobalStyle />
+        <RecoilRoot>
+          <BrowserRouter>
+            <MainRouter />
+          </BrowserRouter>
+        </RecoilRoot>
+      </ThemeProvider>
+    </QueryClientProvider>
+  );
+}
 
 export default App;
