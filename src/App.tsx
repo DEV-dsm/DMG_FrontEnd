d07cf077c1edfe72d1064cb3c58d1dbabc4e4b2c
kimjuwon <juwon2298@naver.com>
2023-06-13 14:15:59 +0900

    👀 :: App 컴포넌트 수정

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,31 @@
-import React from "react";
+import { BrowserRouter } from "react-router-dom";
+import { RecoilRoot } from "recoil";
+import { QueryClientProvider, QueryClient } from "react-query";
+import { GlobalStyle } from "./styles/GlobalStyle";
+import MainRouter from "./router/MainRouter";
 
-const App = () => {
-  return <div>App</div>;
-};
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      retry: 0,
+      staleTime: 1000,
+      refetchInterval: 0,
+      refetchOnWindowFocus: false,
+    },
+  },
+});
+
+function App() {
+  return (
+    <QueryClientProvider client={queryClient}>
+      <GlobalStyle />
+      <RecoilRoot>
+        <BrowserRouter>
+          <MainRouter />
+        </BrowserRouter>
+      </RecoilRoot>
+    </QueryClientProvider>
+  );
+}
 
 export default App;
