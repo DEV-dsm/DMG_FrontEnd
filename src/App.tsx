c1a67a43a61f882bbfd04356558c360c4eaf193c
kimjuwon <juwon2298@naver.com>
2023-06-13 14:11:06 +0900

    머지 좀 하자

--- src/App.tsx
+++ src/App.tsx
@@ -1,36 +1,3 @@
-/* import React from "react";
-import { BrowserRouter } from "react-router-dom";
-import { RecoilRoot } from "recoil";
-import { QueryClientProvider, QueryClient } from "react-query";
-import { GlobalStyle } from "./styles/GlobalStyle";
-import MainRouter from "./router/MainRouter";
-
-const queryClient = new QueryClient({
-  defaultOptions: {
-    queries: {
-      retry: 0,
-      staleTime: 1000,
-      refetchInterval: 0,
-      refetchOnWindowFocus: false,
-    },
-  },
-});
-
-function App() {
-  return (
-    <QueryClientProvider client={queryClient}>
-      <GlobalStyle />
-      <RecoilRoot>
-        <BrowserRouter>
-          <MainRouter />
-        </BrowserRouter>
-      </RecoilRoot>
-    </QueryClientProvider>
-  );
-}
-
-export default App; */
-
 import React from "react";
 
 const App = () => {
