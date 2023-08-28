b36bc15e495771a684973dd70dd85c1efd62f3f4
서유정 <yujeongcheer@gmail.com>
2023-08-28 22:04:57 +0900

    🌼 :: router 라우트 관련

--- src/router/MainRouter.tsx
+++ src/router/MainRouter.tsx
@@ -1,23 +1,31 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import SideBar from "../components/common/Sidebar";
-import Inquire from "../pages/Inquire";
-import SearchUser from "../pages/SearchUser";
-import Messages from "../pages/Messages";
-import MyPage from "../pages/Mypage";
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+
+import SideBar from '../components/common/Sidebar';
+import Inquire from '../pages/Inquire';
+import SearchUser from '../pages/SearchUser';
+import Messages from '../pages/Messages';
+import MyPage from '../pages/Mypage';
+import LogIn from '../pages/auth/LogIn';
+import FindPassword from '../pages/auth/FindPassword';
+import Error404 from '../pages/Error404';
 
 const MainRouter = () => {
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route element={<SideBar />}>
-          <Route path="/inquire" element={<Inquire />} />
-          <Route path="/mypage" element={<MyPage />} />
-          <Route path="/message" element={<Messages />} />
-          <Route path="/searchuser" element={<SearchUser />} />
-        </Route>
-      </Routes>
-    </BrowserRouter>
-  );
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route element={<SideBar />}>
+                    <Route path="/inquire" element={<Inquire />} />
+                    <Route path="/mypage" element={<MyPage />} />
+                    <Route path="/message" element={<Messages />} />
+                    <Route path="/searchuser" element={<SearchUser />} />
+                </Route>
+
+                <Route path="*" element={<Error404 />} />
+                <Route path="/login" element={<LogIn />} />
+                <Route path="/findpassword" element={<FindPassword />} />
+            </Routes>
+        </BrowserRouter>
+    );
 };
 
 export default MainRouter;
