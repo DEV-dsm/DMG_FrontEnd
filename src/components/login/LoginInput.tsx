7c0452dcacd64dd6a5b9004687c64f005c7a3c85
kimjuwon <juwon2298@naver.com>
2023-08-31 22:52:50 +0900

    🗑 :: 안 쓰는 파일 삭제

--- src/components/login/LoginInput.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-function LoginInput() {
-  return <div>LoginInput</div>;
-}
-
-export default LoginInput;
--- src/components/mypage/TestPage.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-function TestPage() {
-  return <div>TestPage</div>;
-}
-
-export default TestPage;
