bafb0758b0462f4f68eee179b9bbf19b7b615b6e
kimjuwon <juwon2298@naver.com>
2023-09-19 08:22:03 +0900

    📷 :: assets 에셋 변경 ( 사진, 관련 코드 )
    모듈 변경

--- src/assets/icons/index.ts
+++ src/assets/icons/index.ts
@@ -1,13 +1,13 @@
-import MyPageBlackIcons from "./beforeProfile.png";
-import SearchUserBlackIcons from "./beforeSearchUser.png";
-import MessageBlackIcons from "./beforeChatting.png";
-import RequireBlackIcons from "./beforeWarning.png";
-import LogOutBlackIcons from "./beforeLogOut.png";
-import MyPageWhiteIcons from "./afterProfile.png";
-import SearchWhiteIcons from "./afterSearchUser.png";
-import MessageWhiteIcons from "./afterChatting.svg";
-import RequireWhiteIcons from "./afterWarning.png";
-import LogOutWhiteIcons from "./afterLogOut.png";
+import MyPageBlackIcons from './beforeProfile.png';
+import SearchUserBlackIcons from './beforeSearchUser.png';
+import MessageWhiteIcons from './beforeChatting.png';
+import RequireBlackIcons from './beforeWarning.png';
+import LogOutBlackIcons from './beforeLogOut.png';
+import MyPageWhiteIcons from './afterProfile.png';
+import SearchWhiteIcons from './afterSearchUser.png';
+import MessageBlackIcons from './afterChatting.svg';
+import RequireWhiteIcons from './afterWarning.png';
+import LogOutWhiteIcons from './afterLogOut.png';
 
 export const Icons = {
   MyPageBlackIcons,
