8e6a87f7a1076d08e5227aa97f8a7457ee85df3e
kimjuwon <juwon2298@naver.com>
2023-10-09 01:15:41 +0900

    ✨ :: input Atom 추가

--- src/atom/authAtom.ts
+++ src/atom/authAtom.ts
@@ -1,9 +1,18 @@
 import { atom } from 'recoil';
+import { findPWInputType } from '../models/auth';
 
 export const loginInputsAtom = atom({
   key: 'LoginInputsAtom',
   default: {
     identify: '',
     password: '',
+  },
+});
+
+export const findPwInputsAtom = atom<findPWInputType>({
+  key: 'findPwInputsAtom',
+  default: {
+    email: '',
+    authnumber: 0,
   },
 });
