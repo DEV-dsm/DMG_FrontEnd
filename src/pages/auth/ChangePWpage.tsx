91c0a2814ef4acff92dae4f3c67eb1aef0099f49
juwon <juwon2298@naver.com>
2023-10-19 22:39:01 +0900

    ✨ :: 비밀번호 확인 기능 추가

--- src/pages/auth/ChangePWpage.tsx
+++ src/pages/auth/ChangePWpage.tsx
@@ -7,19 +7,28 @@ import CheckImgs from '../../assets/auth/clickCheck.png';
 import { useRecoilValue } from 'recoil';
 import { ChangePWInputsAtom } from '../../atom/authAtom';
 import { useChangePW } from '../../components/hooks/apis/useChangePW';
+import toast from 'react-hot-toast';
 
 const ChangePWPage = () => {
   const navigate = useNavigate();
   const inputData = useRecoilValue(ChangePWInputsAtom);
   const { mutate } = useChangePW(inputData);
 
+  const ChangePWClick = () => {
+    if (inputData.password !== inputData.passwordCheck) {
+      toast.error('비밀번호가 일치하지 않습니다.');
+      return;
+    }
+    mutate();
+  };
+
   return (
     <S.Wrapper>
       <S.Container>
-        <S.Title>Modify Password</S.Title>
+        <S.Title>Modify Password</S.Title>5
         <ChangePWInput />
         <ButtonContainer>
-          <AuthButton text="Change" width="50%" onClick={() => mutate()} />
+          <AuthButton text="Change" width="50%" onClick={() => ChangePWClick()} />
           <CheckImg src={CheckImgs} />
         </ButtonContainer>
         <S.Link onClick={() => navigate('/')}>Go Login</S.Link>
