cf948059d77f6671bb075b3d38c412e273288fae
juwon <juwon2298@naver.com>
2023-11-21 19:28:01 +0900

    🐛 :: 불필요한 코드 삭제

--- src/components/mypage/UserInfoInput.tsx
+++ src/components/mypage/UserInfoInput.tsx
@@ -1,4 +1,3 @@
-import { useState } from 'react';
 import styled from 'styled-components';
 
 import Input from '../common/Input';
@@ -6,19 +5,9 @@ import { UserInfo, UserInfoDetail } from '../../constants';
 import { IUserInfoDetailType } from '../../models/Mypage';
 import { Images } from '../../assets/mypage';
 
-const regexp = /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/;
+// const regexp = /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/;
 
 const UserInfoInput = () => {
-  const [password, setPassword] = useState<string>('');
-  const [isValidPassword, setIsValidPassword] = useState<boolean>(false);
-
-  // 정규식 검사
-  const handlePasswordChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    const inputPassword = e.target.value;
-    setPassword(inputPassword);
-    setIsValidPassword(regexp.test(inputPassword));
-  };
-
   return (
     <Container>
       {UserInfo.map((item: IUserInfoDetailType, index: number) => (
@@ -31,23 +20,16 @@ const UserInfoInput = () => {
           <Input
             type={item.name === 'Password' ? 'password' : 'text'}
             name={item.name}
-            onChange={item.name === 'Password' ? handlePasswordChange : undefined}
             placeholder={item.placeholder}
           />
           {item.id === 4 ? <BlackLock src={Images.blackCloseLock} alt="" /> : ''}
           {item.id === 5 ? <BlackCheck src={Images.blackBeforeCheck} alt="" /> : ''}
           <Input
             type={item.name === 'Password' ? 'password' : 'text'}
-            onChange={item.name === 'Password' ? handlePasswordChange : undefined}
             placeholder={item.otherplaceholder}
           />
           {item.id === 4 ? <BlackLock2 src={Images.blackCloseLock} alt="" /> : ''}
           {item.id === 5 ? <BlackCheck2 src={Images.blackBeforeCheck} alt="" /> : ''}
-          {item.name === 'Password' && !isValidPassword && password.length > 0 && (
-            <WarningMessage>
-              비밀번호는 최소 8자리이며, 영문자와 숫자를 포함해야 합니다.
-            </WarningMessage>
-          )}
         </DetailInputWrapper>
       ))}
     </Container>
@@ -65,12 +47,7 @@ const DetailInputWrapper = styled.div`
   display: flex;
   flex-direction: column;
   position: relative;
-`;
-
-const WarningMessage = styled.div`
-  font-size: 12px;
-  color: #e22a22;
-  margin-top: 5px;
+  gap: 7px;
 `;
 
 const CommonStyles = `
@@ -78,7 +55,7 @@ const CommonStyles = `
   width: 16px;
   height: 16px;          
   position: absolute;
-  top: 8px;
+  top: 10px;
   right: 15px;
 `;
 
@@ -88,12 +65,12 @@ const BlackLock = styled.img`
 
 const BlackLock2 = styled.img`
   ${CommonStyles}
-  top: 47px;
+  top: 55px;
 `;
 
 const BlackCheck = styled.img`
   ${CommonStyles}
-  top: 47px;
+  top: 55px;
 `;
 
 const BlackCheck2 = styled.img`
