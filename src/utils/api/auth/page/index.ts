6427cb1ec45653cb293ff362d98d9cca3d65134d
juwon <juwon2298@naver.com>
2023-12-12 00:57:47 +0900

    👀 :: api Data 코드 수정

--- src/utils/api/auth/page/index.ts
+++ src/utils/api/auth/page/index.ts
@@ -2,9 +2,6 @@ import { IQuestionInputDataType } from '../../../../models/auth';
 import instance from '../../../axios';
 
 export const userQuestion = async (inputsData: IQuestionInputDataType) => {
-  const response = await instance.post('/user/question', {
-    title: inputsData.title,
-    content: inputsData.content,
-  });
+  const response = await instance.post('/user/question', inputsData);
   return response;
 };
