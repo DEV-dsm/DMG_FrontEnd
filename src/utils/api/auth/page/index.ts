98f6011ddb123e3628013ff364e2fc40b3a6b201
juwon <juwon2298@naver.com>
2023-12-16 19:59:57 +0900

    ✨ :: 개인 프로필 조회 api 연동

--- src/utils/api/auth/page/index.ts
+++ src/utils/api/auth/page/index.ts
@@ -1,7 +1,22 @@
-import { IQuestionInputDataType } from '../../../../models/auth';
+import { useRecoilValue } from 'recoil';
+import { IGetIndividualUserListDataType, IQuestionInputDataType } from '../../../../models/auth';
 import instance from '../../../axios';
+import { userIdAtom } from '../../../../atom/authAtom';
+import { useQuery } from 'react-query';
 
 export const userQuestion = async (inputsData: IQuestionInputDataType) => {
   const response = await instance.post('/user/question', inputsData);
   return response;
+};
+
+export const GetIndividualUserList = () => {
+  const userID = useRecoilValue(userIdAtom);
+  const response = async () => {
+    const { data } = await instance.get<IGetIndividualUserListDataType>(
+      `/profile/student/${userID}`
+    );
+    console.log(data);
+    return data?.data;
+  };
+  return useQuery(['GetIndividualUserList'], response);
 };
