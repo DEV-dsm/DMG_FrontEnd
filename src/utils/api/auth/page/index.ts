1638580271b3b5f147d8272bb510a27a7b622720
juwon <juwon2298@naver.com>
2023-12-18 11:19:52 +0900

    ✨ :: 비밀번호 수정 api 연동

--- src/utils/api/auth/page/index.ts
+++ src/utils/api/auth/page/index.ts
@@ -5,18 +5,24 @@ import { userIdAtom } from '../../../../atom/authAtom';
 import { useQuery } from 'react-query';
 
 export const userQuestion = async (inputsData: IQuestionInputDataType) => {
-  const response = await instance.post('/user/question', inputsData);
-  return response;
+  return await instance.post('/user/question', inputsData);
 };
 
 export const GetIndividualUserList = () => {
   const userID = useRecoilValue(userIdAtom);
   const response = async () => {
     const { data } = await instance.get<IGetIndividualUserListDataType>(
-      `/profile/student/${userID}`
+      `/profile/student/${userID}`,
     );
     console.log(data);
     return data?.data;
   };
   return useQuery(['GetIndividualUserList'], response);
+};
+
+export const userChangePwData = async (password: string, newPassword: string) => {
+  return await instance.patch('/user/password', {
+    password,
+    newPassword,
+  });
 };
