76e8d8e205433a7a6c992df088d57d9d25d25e30
juwon <juwon2298@naver.com>
2023-12-18 11:21:22 +0900

    ✨ :: 비밀번호 수정 api hook 추가

---
+++ src/components/hooks/apis/useFindPW.ts
@@ -0,0 +1,23 @@
+import { useMutation } from 'react-query';
+import { AxiosError } from 'axios';
+import { IChangePWInputType } from '../../../models/auth';
+import { userChangePassword } from '../../../utils/api/auth';
+import toast from 'react-hot-toast';
+
+export const useFindPW = (inputData: IChangePWInputType) => {
+  return useMutation(() => userChangePassword(inputData), {
+    onSuccess: () => {
+      toast.success('비밀번호가 수정되었습니다.', { duration: 1500 });
+    },
+    onError: (error: AxiosError) => {
+      switch (error.response?.status) {
+        case 404:
+          toast.error('존재하지 않는 유저입니다.', { duration: 1500 });
+          break;
+        case 409:
+          toast.error('비밀번호 형식에 알맞지 않습니다.', { duration: 1500 });
+          break;
+      }
+    },
+  });
+};
