b891760a8183297e623526cd0c09789ccb21435d
juwon <juwon2298@naver.com>
2023-12-30 20:21:17 +0900

    ✍ :: useForm tabsize 2로 변경

--- src/components/hooks/useForm.ts
+++ src/components/hooks/useForm.ts
@@ -1,17 +1,15 @@
-import { useState } from "react";
+import { useState } from 'react';
 
 export function useForm<T>(initialState: T) {
-    const [form, setForm] = useState<T>(initialState);
+  const [form, setForm] = useState<T>(initialState);
 
-    const handleChange = (
-        e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
-    ) => {
-        const { name, value } = e.target;
-        setForm((prevForm) => ({
-            ...prevForm,
-            [name]: value,
-        }));
-    };
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
+    const { name, value } = e.target;
+    setForm((prevForm) => ({
+      ...prevForm,
+      [name]: value,
+    }));
+  };
 
-    return { form, setForm, handleChange };
+  return { form, setForm, handleChange };
 }
